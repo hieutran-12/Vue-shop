ff84cd5b21adcb0c5cf021efb4a13cf51ceb67fe
Hieu Tran <hieuminhtran204@gmail.com>
2025-06-16 17:14:20 +0700

    Đẩy code Vue đầu tiên

---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+ 
+  <div>
+ <v-main>
+  <AppHeader />
+
+    <router-view />
+  </v-main>
+
+  <AppFooter />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
---
+++ src/pages/About.vue
@@ -0,0 +1,10 @@
+
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h1>huhuh</h1>
+        <Button/>
+    </div>
+</template>
